7984009a53a8c1abd9163ebdd6a49e2dc71755df
DESKTOP-75401JP\mclea <mcleancolinm@gmail.com>
2021-09-06 12:07:02 -0500

    Initial commit

---
+++ 01_Start_Building/MatchGame/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MatchGame
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            SetupGame();
+        }
+
+        void SetupGame()
+        {
+            List<string> animalEmojis = new List<string>()
+            {
+                "🐵", "🐵",
+                "🐶", "🐶",
+                "🦊", "🦊",
+                "🦒", "🦒",
+                "🐯", "🐯",
+                "🦁", "🦁",
+                "🐱", "🐱",
+                "🐺", "🐺"
+            };
+
+            Random random = new Random();
+            foreach (TextBlock textBlock in mainGrid.Children.OfType<TextBlock>())
+            {
+                int i = random.Next(animalEmojis.Count);
+                string nextEmoji = animalEmojis[i];
+                textBlock.Text = nextEmoji;
+                animalEmojis.RemoveAt(i);
+            }
+        }
+    }
+}
