b4515adf8d0f2fda5fcc3dba9d15f8f8a1ac7f86
DESKTOP-75401JP\mclea <mcleancolinm@gmail.com>
2021-09-13 16:57:12 -0500

    Complete the experimental controls example

--- 02_Dive_Into_CSharp/ControlExperiment/MainWindow.xaml.cs
+++ 02_Dive_Into_CSharp/ControlExperiment/MainWindow.xaml.cs
@@ -24,5 +24,49 @@ namespace ControlExperiment
         {
             InitializeComponent();
         }
+
+        private void enterNumberBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            displayNumberBlock.Text = enterNumberBox.Text;
+        }
+
+        private void enterNumberBox_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            e.Handled = !int.TryParse(e.Text, out int result);
+        }
+
+        private void smallSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            displayNumberBlock.Text = smallSlider.Value.ToString("0");
+        }
+
+        private void bigSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            displayNumberBlock.Text = bigSlider.Value.ToString("000-000-0000");
+        }
+
+        private void RadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+            if (sender is RadioButton radioButton)
+                displayNumberBlock.Text = radioButton.Content.ToString();
+        }
+
+        private void ListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (myListBox.SelectedItem is ListBoxItem listBoxItem)
+                displayNumberBlock.Text = listBoxItem.Content.ToString();
+        }
+
+        private void readOnlyComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (readOnlyComboBox.SelectedItem is ListBoxItem listBoxItem)
+                displayNumberBlock.Text = listBoxItem.Content.ToString();
+        }
+
+        private void editableComboBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (sender is ComboBox comboBox)
+                displayNumberBlock.Text = comboBox.Text;
+        }
     }
 }
